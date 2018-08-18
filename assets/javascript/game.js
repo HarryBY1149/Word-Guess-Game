0dc61e34673dc874dd9107866ef4d2e802f9b722
H. B. Young <HarryBeckerYoung@gmail.com>
2018-08-18 09:32:58 -0400

    Functionality and styling, still working on some bells and whistles

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,68 +3,77 @@
 
 
 
-var wins = 0;
+
 
 function newGame() {
-    var breeds = ["AKITA", "MALAMUTE", "BASSETHOUND", "BEAGLE", "CHIHUAHUA", "TERRIER", "DALMATION", "DOBERMANPINSCHER", "GERMANSHEPHERD", "POINTER"];
-    
-    var guesses = 20;
-    var currentWord = breeds[Math.floor(Math.random() * breeds.length)];
+    var breeds = ["AKITA", "MALAMUTE", "BASSETHOUND", "BEAGLE", "CHIHUAHUA", "TERRIER", "DALMATION", "DOBERMANPINSCHER", "GERMANSHEPHERD", "POINTER", "JINDO", "SAINTBERNARD", "MASTIFF", "IRISHWOLFHOUND", "GREYHOUND", "AUSTRALIANSHEPHERD", "GOLDENRETRIEVER"];
+    var currentWord = "";
+    var guesses = 10;
     var answerArray = [];
     var currentWordArray = [];
     var lettersGuessedArray = [];
-   
-
+    var wins = 0;
     var displayWins = document.getElementById("display-wins");
     var guessField = document.getElementById("guess-field");
     var guessesRemaining = document.getElementById("guesses-remaining");
     var lettersGuessed = document.getElementById("letters-guessed");
-    displayWins.textContent=wins;
-    for (i = 0; i < currentWord.length; i++) {
-        var space = "_";
-        answerArray.push(space);
-        guessField.textContent = answerArray.join("");
-    };
+    //var happyDog = document.getElementById("happy-dog");
+    function reset() {
+        answerArray = [];
+        currentWord = "";
+        currentWordArray = []
+        lettersGuessedArray = [];
+        guesses = 10;
+        displayWins.textContent = wins;
+        lettersGuessed.textContent = lettersGuessedArray;
+        guessesRemaining.textContent = guesses;
+        function generateVariables() {
+            currentWord = breeds[Math.floor(Math.random() * breeds.length)];
+            for (i = 0; i < currentWord.length; i++) {
+                var space = "_";
+                answerArray.push(space);
+                guessField.textContent = answerArray.join("");
+            };
 
-    for (j = 0; j < currentWord.length; j++) {
-        var letter = currentWord.charAt(j);
-        currentWordArray.push(letter);
-    };
+            for (j = 0; j < currentWord.length; j++) {
+                var letter = currentWord.charAt(j);
+                currentWordArray.push(letter);
+            };
+        }
+        generateVariables();
+    }
+
+    reset()
     document.onkeyup = function (event) {
         var userGuess = event.key.toUpperCase();
         var matches = 0;
-        
-        currentWordArray.forEach(function (letter, index) {
-            if (letter === userGuess) {
-                answerArray[index] = currentWordArray[index];
-                matches++;
+        if (lettersGuessedArray.indexOf(userGuess) === -1) {
+            lettersGuessedArray.push(userGuess);
+            lettersGuessed.textContent = lettersGuessedArray.join("");
+            currentWordArray.forEach(function (letter, index) {
+                if (letter === userGuess) {
+                    answerArray[index] = currentWordArray[index];
+                    guessField.textContent = answerArray.join("");
+                    matches++;
+                    if (answerArray.indexOf('_') === -1) {
+                        alert("You Win!");
+                        wins++;
+                        //happyDog.setAttribute("Style", "Opacity:1;");
+                        reset();
+                    };
+                };
+
+            })
+            if (matches === 0) {
+                guesses--;
+                guessesRemaining.textContent = guesses;
+                if (guesses === 0) {
+                    alert("You Lose");
+                    reset();
+                };
+
             };
-        })
-        if (matches === 0) {
-            guesses--;
-        };
-        lettersGuessedArray.push(userGuess);
-
-        guessField.textContent = answerArray.join("");
-        guessesRemaining.textContent = guesses;
-        lettersGuessed.textContent = lettersGuessedArray.join("");
-        
-
-        if (answerArray.indexOf('_') === -1) {
-            alert("You Win!");
-            wins++;
-            lettersGuessed.textContent = "";
-            guessesRemaining.textContent = 20;
-            
-            newGame();
-        };
-        if (guesses === 0) {
-            alert("You Lose");
-            lettersGuessed.textContent = "";
-            guessesRemaining.textContent = 20;
-            newGame();
-        };
-
+        }
     };
 }
 
